e58c2fa0850c55a5edf3f3acdcc4c28bdfb47011
bibin <bibinkurian2@gmail.com>
2020-04-12 18:58:29 +0000

    dce first draft

--- simple/p3/C++/CSE.cpp
+++ simple/p3/C++/CSE.cpp
@@ -37,10 +37,135 @@ int CSELdElim=0;
 int CSELdStElim=0;
 int CSERStElim=0;
 
+bool isDead(Instruction &I)
+{
+  int opcode = I.getOpcode();
+  switch(opcode){
+  case Instruction::Add:
+  case Instruction::FNeg:
+  case Instruction::FAdd: 	
+  case Instruction::Sub:
+  case Instruction::FSub: 	
+  case Instruction::Mul:
+  case Instruction::FMul: 	
+  case Instruction::UDiv:	
+  case Instruction::SDiv:	
+  case Instruction::FDiv:	
+  case Instruction::URem: 	
+  case Instruction::SRem: 	
+  case Instruction::FRem: 	
+  case Instruction::Shl: 	
+  case Instruction::LShr: 	
+  case Instruction::AShr: 	
+  case Instruction::And: 	
+  case Instruction::Or: 	
+  case Instruction::Xor: 	
+  case Instruction::Alloca:
+  case Instruction::GetElementPtr: 	
+  case Instruction::Trunc: 	
+  case Instruction::ZExt: 	
+  case Instruction::SExt: 	
+  case Instruction::FPToUI: 	
+  case Instruction::FPToSI: 	
+  case Instruction::UIToFP: 	
+  case Instruction::SIToFP: 	
+  case Instruction::FPTrunc: 	
+  case Instruction::FPExt: 	
+  case Instruction::PtrToInt: 	
+  case Instruction::IntToPtr: 	
+  case Instruction::BitCast: 	
+  case Instruction::AddrSpaceCast: 	
+  case Instruction::ICmp: 	
+  case Instruction::FCmp: 	
+  case Instruction::PHI: 
+  case Instruction::Select: 
+  case Instruction::ExtractElement: 	
+  case Instruction::InsertElement: 	
+  case Instruction::ShuffleVector: 	
+  case Instruction::ExtractValue: 	
+  case Instruction::InsertValue: 
+    if ( I.use_begin() == I.use_end() )
+    {
+      return true;
+    }    
+    break;
+
+  case Instruction::Load:
+    {
+      LoadInst *li = dyn_cast<LoadInst>(&I);
+      if (li->isVolatile())
+	      return false;
+
+      if ( I.use_begin() == I.use_end() )
+      {
+        return true;
+      }
+      
+      break;
+    }
+  
+  default: // any other opcode fails (includes stores and branches)
+    // we don't know about this case, so conservatively fail!
+    return false;
+  }
+  
+  return false;
+}
+
 void LLVMCommonSubexpressionElimination_Cpp(Module *M)
 {
   // for each function, f:
-  //   FunctionCSE(f);
+  //   FunctionCSE(f);  
+ 
+  for(auto f = M->begin(); f!=M->end(); f++)       // loop over functions
+  {
+    std::set<Instruction*> worklist;
+
+    for(auto bb= f->begin(); bb!=f->end(); bb++)
+    {
+  // loop over basic blocks
+      for(auto i = bb->begin(); i != bb->end(); i++)
+      {
+        //loop over instructions
+        if (isDead(*i)) 
+        {
+          //add I to a worklist to replace later
+          worklist.insert(&*i);
+        }	      
+      }
+    }
+
+    while(worklist.size()>0) 
+    {
+      // Get the first item 
+      Instruction *i = *(worklist.begin());
+      // Erase it from worklist
+      worklist.erase(i);
+      
+      if(isDead(*i))
+      {
+        for(unsigned op=0; op<i->getNumOperands(); op++)
+        {
+          // Note, op still has one use (in i) so the isDead routine
+          // would return false, so we’d better not check that yet.
+          // This forces us to check in the if statement above.
+          
+          
+          
+          // The operand could be many different things, in 
+          // particular constants. Don’t try to delete them
+          // unless they are an instruction:
+          if ( isa<Instruction>(i->getOperand(op)) ) 
+          {
+            Instruction *o = dyn_cast<Instruction>(i->getOperand(op));
+            worklist.insert(o);
+          }
+        }
+        i->eraseFromParent();
+        CSEDead++;
+      }        
+    }
+  }  
  
   // print out summary of results
   fprintf(stderr,"CSE_Dead.....%d\n", CSEDead);
